9bef09645569e6f7d4b90cfc17c61d6ee98b968d
Dale Stevens <dale@twilightcoders.net>
2018-01-19 13:01:02 -0700

    Simpler check

--- lib/erlang_parser/elements/tuple.rb
+++ lib/erlang_parser/elements/tuple.rb
@@ -4,7 +4,7 @@ module ErlangParser
       def to_ruby
         super
         arr = @elements.map {|el|
-          (el.is_a?(Term) || el.is_a?(Enum))? el.to_ruby : el
+          el.is_a?(Term) ? el.to_ruby : el
         }
         key = arr.delete_at(0)
         hash = {key => ( arr.length>1 ? arr: arr[0])}
